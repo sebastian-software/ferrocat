bcb66ee4465d24cd74ae6149bfc806c5beeacd17
Sebastian Werner <s.werner@sebastian-software.de>
2026-07-03 09:38:24 +0200

    ci(api): normalize public API std io paths

--- scripts/check-public-api-snapshots.sh
+++ scripts/check-public-api-snapshots.sh
@@ -14,6 +14,8 @@ for package in "${packages[@]}"; do
   snapshot="api-snapshots/${package}.txt"
   generated="${tmpdir}/${package}.txt"
 
-  cargo +nightly public-api -p "$package" --all-features -sss --color=never > "$generated"
+  cargo +nightly public-api -p "$package" --all-features -sss --color=never \
+    | sed 's/core::io::error::Error/std::io::error::Error/g' \
+    > "$generated"
   diff -u "$snapshot" "$generated"
 done
